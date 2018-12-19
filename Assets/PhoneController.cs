3410e007f4e37542ca0d0bf1f5931f66646ea803
Benjamin Harbakk <stealcase@hotmail.com>
2018-12-19 17:52:39 +0100

    Initial Commit

---
+++ Assets/PhoneController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PhoneController : MonoBehaviour
+{
+    bool up = false;
+    RectTransform canvas;
+    RectTransform phone;
+    Vector3 startingPosition;
+    public float speed;
+    public float ypos;
+
+    private void Start()
+    {
+        phone = gameObject.GetComponent<RectTransform>();
+        canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        startingPosition = transform.position;
+        speed = -10f;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && up == false)
+        {
+            up = true;
+            print("mousewheeled");
+            transform.position = new Vector3(startingPosition.x, ypos, startingPosition.z);
+            return;
+        }
+        if (Input.GetButtonDown("Fire1") && up == true)
+        {
+            up = false;
+            transform.position = startingPosition;
+            print("phone going down");
+            return;
+
+            }
+
+
+    }
+}
